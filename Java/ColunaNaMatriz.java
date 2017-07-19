f6a1bf9424845a57e468bc4cf5d0767b2427e217
Gustavo Dolmen Reche <gustavodreche@msn.com>
2017-07-19 18:44:47 -0300

    Add files via upload

---
+++ Java/ColunaNaMatriz.java
@@ -0,0 +1,31 @@
+package URI;
+
+import java.util.Scanner;
+
+public class ColunaNaMatriz {
+
+	public static void main(String[] args) {
+				Scanner entrada = new Scanner (System.in);
+				double m1 [][] = new double [12][12];
+				double soma = 0;
+				int ver = Integer.parseInt(entrada.nextLine());
+				String oper = entrada.nextLine();
+				for (int l = 0; l < 12; l++){
+					for (int c = 0; c < 12; c++){
+						m1[l][c] = Double.parseDouble(entrada.nextLine());
+					}
+				}
+				for (int i = 0; i < 12; i++){
+					if (oper.equals("S")){
+						soma = soma + m1[i][ver];
+					}
+					else{
+						soma = soma + m1[i][ver];
+					}
+				}
+				if (oper.equals("M")){
+					soma = soma / 12;
+				}
+				System.out.println(String.format("%.1f", soma));
+			}
+		}
